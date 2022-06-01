0f1f6f3aee1078892efc55d41a5743b3bcdd9cab
shree <shree@192.168.1.7>
2022-06-01 15:04:18 +0530

    Second Commit

---
+++ Day2_JPA CRUD/src/com/cg/client/Client.java
@@ -0,0 +1,34 @@
+package com.cg.client;
+
+import com.cg.entities.Student;
+import com.cg.service.StudentService;
+import com.cg.service.StudentServiceImpl;
+
+public class Client {
+
+	public static void main(String[] args) {
+		StudentService obj=new StudentServiceImpl();
+		Student student=new Student();
+		
+		//create 
+		student.setStudentId(104);
+		student.setName("Vipul");
+		obj.addStudent(student);
+		
+		/*//retrieve
+		student=obj.findStudentById(101);
+		System.out.println("Student Id: "+student.getStudentId());
+		System.out.println("Student Name: "+student.getName());*/
+		
+		/*//update
+		student=obj.findStudentById(101);
+		student.setName("Pratik Thakare");
+		obj.updateStudent(student);*/
+		
+		//delete
+		student=obj.findStudentById(102);
+		obj.removeStudent(student);
+
+	}
+
+}
